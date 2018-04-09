b6a7f2653d372df076515c1d3d17815c3934f442
Mazahir Hashimli <mazahir@tinymesh.com>
2018-04-09 11:08:58 +0200

    Working on T328 and T330 Mocking Client Server on Bootstrap.

--- src/main/java/com/tinymesh/vicinity/adapter/bootstrap/ObjectBootstrap.java
+++ src/main/java/com/tinymesh/vicinity/adapter/bootstrap/ObjectBootstrap.java
@@ -4,6 +4,7 @@ import com.tinymesh.vicinity.adapter.client.TinyMClient;
 import com.tinymesh.vicinity.adapter.database.Device;
 import com.tinymesh.vicinity.adapter.jsonmodels.DoorSensorJSON;
 import org.mockserver.client.server.MockServerClient;
+import org.mockserver.integration.ClientAndServer;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -29,7 +30,7 @@ public class ObjectBootstrap implements ApplicationListener<ContextRefreshedEven
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
-        getDoorsFromAPI();
+        //getDoorsFromAPI();
     }
 
     private void createDeviceObjects(){
@@ -46,37 +47,5 @@ public class ObjectBootstrap implements ApplicationListener<ContextRefreshedEven
 
     }
 
-    private void getDoorsFromAPI(){
-        new MockServerClient("localhost", 1080)
-                .when(request()
-                        .withMethod("GET")
-                        .withPath("/headers"))
-                .respond(response()
-                        .withStatusCode(200)
-                        .withBody(json(
-                                "{\n" +
-                                        "  \"key\" : \"gidKaYtHvUVOo\",\n" +
-                                        "  \"location\" : [],                \n" +
-                                        "  \"proto/tm\" : {                  \n" +
-                                        "    \"firmware\" : \"2.01\",\n" +
-                                        "    \"hardware\" : \"2.00\"\n" +
-                                        "  },\n" +
-                                        "  \"network\" : \"T\",                \n" +
-                                        "  \"type\" : \"building-sensor-v3\", \n" +
-                                        "  \"address\" : 1074135040,      \n" +
-                                        "  \"name\" : \"Test B - 119 sek CO2, 10 min intervall\",\n" +
-                                        "  \"provisioned\" : \"active\",   \n" +
-                                        "  \"meta\" : {\n" +
-                                        "    \"updated\" : \"2018-03-19T09:53:50.760Z\",\n" +
-                                        "    \"event/date\" : \"2017-06-23T11:20:52.446Z\",  \n" +
-                                        "    \"created\" : \"2017-03-17T15:17:58.369Z\",     \n" +
-                                        "    \"event/key\" : \"1eq-6jok-djhg-511-VC9naWRLYVl0SHZVVk9v\" \n" +
-                                        "  }\n" +
-                                        "}"))
-                );
-        DoorSensorJSON doorSensorJSON = tinyMClient.getDoorSenors();
-        if (doorSensorJSON != null){
-            Device device = new Device("Device1", "Sensor", UUID.randomUUID(), LocalDateTime.now(),true,"www.test.com");
-        }
-    }
+
 }
