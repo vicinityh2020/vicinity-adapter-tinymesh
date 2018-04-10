98562468e20e0bdb3e9988950277e34271fffa3a
Dmitry <dmitry@tiny-mesh.com>
2018-04-10 11:19:12 +0200

    Fix to the dateTime type in PropertyValue

--- src/main/java/com/tinymesh/vicinity/adapter/model/PropertyValue.java
+++ src/main/java/com/tinymesh/vicinity/adapter/model/PropertyValue.java
@@ -3,7 +3,7 @@ package com.tinymesh.vicinity.adapter.model;
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
-import org.threeten.bp.OffsetDateTime;
+import java.time.OffsetDateTime;
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
