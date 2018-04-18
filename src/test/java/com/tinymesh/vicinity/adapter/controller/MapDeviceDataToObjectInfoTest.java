6ea58b258b35469b95a7daacfedfd2f36edbf2cc
Dmitry <dmitry@tiny-mesh.com>
2018-04-18 12:28:19 +0200

    Refactored DeviceTest to be MapDeviceDataToObjectInfoTest to reflect what is being tested and added more test cases.

---
+++ src/test/java/com/tinymesh/vicinity/adapter/controller/MapDeviceDataToObjectInfoTest.java
@@ -0,0 +1,51 @@
+package com.tinymesh.vicinity.adapter.controller;
+
+import com.tinymesh.vicinity.adapter.entity.Device;
+import com.tinymesh.vicinity.adapter.model.ObjectInfo;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static com.tinymesh.vicinity.adapter.controller.ObjectsApiController.mapDeviceDataToObjectInfo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class MapDeviceDataToObjectInfoTest {
+    private List<Device> deviceList;
+    private List<ObjectInfo> objectInfoList;
+
+    @Before
+    public void setup() {
+        deviceList = new ArrayList<>();
+        deviceList.add(new Device("Device1","Sensor", UUID.randomUUID(), LocalDateTime.now(), true, "www.test.com", 1));
+    }
+
+    @Test
+    public void testMapDeviceDataToObjectInfoCorrectMapping() {
+        objectInfoList = mapDeviceDataToObjectInfo(deviceList);
+
+        assertEquals(deviceList.get(0).getDeviceName(), objectInfoList.get(0).getName());
+        assertEquals(deviceList.get(0).getDeviceType(), objectInfoList.get(0).getType());
+        assertEquals(deviceList.get(0).getUuid(), objectInfoList.get(0).getOid());
+    }
+
+    @Test
+    public void testMapDeviceDataToObjectInfoReturnsNonEmptyList() {
+        objectInfoList = mapDeviceDataToObjectInfo(deviceList);
+
+        assertNotNull(objectInfoList);
+        assertEquals(1, objectInfoList.size());
+    }
+
+    @Test
+    public void testMapDeviceDataToObjectInfoEmptyInput(){
+        objectInfoList = mapDeviceDataToObjectInfo(deviceList);
+
+        assertNotNull(objectInfoList);
+        assertEquals(0, objectInfoList.size());
+    }
+}
